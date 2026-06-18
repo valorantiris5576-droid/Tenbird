cb3e6c95fb79bcc7fa7abde511cbbfbc37d49080
tenbird <Vynix8625@gmail.com>
2026-06-19 08:02:10 +0900

    fix: 프로필 화면 괄호 오류 수정 및 디버깅 완료

--- lib/screens/challenge_screen.dart
+++ lib/screens/challenge_screen.dart
@@ -2,24 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChallengeScreen extends StatelessWidget{
-  const ChallengeScreen([super.key]);
-
-  @override
-  Widget build(BuildContext context) {
-    final uid = FirebaseAuth.instance.currentUser?.uid;
-
-    return Scaffold(
-      backgroundColor: const Color(0xFF0A0E1A),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal : 20, vertica: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children : []
-          )
-        )
-      )
-    )
-  }
-}
